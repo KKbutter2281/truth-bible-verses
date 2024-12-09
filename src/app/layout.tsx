0539be8a085012d00a8973fc91b0ca285696b01d
KKbutter2281 <kalebarora@outlook.com>
2024-12-09 17:28:13 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,22 @@
-import type { Metadata, Viewport } from "next";
-import "./globals.css";
-import { DM_Sans, DM_Mono } from "next/font/google";
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-const sans = DM_Sans({
-  variable: "--font-dm-sans",
-});
-const mono = DM_Mono({
-  variable: "--font-dm-mono",
-  weight: "400",
-});
+const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: "Sahara Compute",
-  description:
-    "Sahara Compute offers a range of fully managed IT solutions, hosting, and cloud services for businesses of all sizes.",
-};
-
-export const viewport: Viewport = {
-  themeColor: "#f59e0b",
-  colorScheme: "only light",
-};
+export const metadata = {
+  title: 'Biblical Truths',
+  description: 'Exploring the Power of Truth in Scripture',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
-      <body
-        className={`${sans.variable} ${mono.variable} antialiased font-sans text-lg`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
+
